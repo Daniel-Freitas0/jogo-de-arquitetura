973d8df27c554e9cae05af0bb913c44b64d4176f
Daniel-Freitas0 <daniel.siqueira@sptech.school>
2022-05-15 12:47:18 -0300

    Upload

---
+++ js/perguntas.js
@@ -0,0 +1,100 @@
+
+
+// Array of Objects
+const quiz = [
+    {
+        q:'Acesso aleatorio de memoria, memoria temporaria em que o processador pode utilizar em um curto periodo de tempo.',
+        options:['RAM','ROM','AAM','Flash'],
+        answer:0
+    },
+
+    {
+        q:'Uma parte da CPU que executa operações logicas e aritméticas.',
+        options:['AL','LA','MLP','ULA'],
+        answer:3
+    },
+
+    {
+        q:'Unidade de processamento central de um computador, basicamente um "cérebro" responsavel por processar e executar instruções',
+        options:['CPU','Placa','UPC','RGB'],
+        answer:0
+    },
+
+    {
+        q:'Memória permanete ou semi permante',
+        options:['MP','Cache','ROM','MID'],
+        answer:2
+    },
+
+    {
+        q:'Unidades de memoria de alta velocidade',
+        options:['Flash','Cache','Registradores','ROM'],
+        answer:2
+    },
+    
+    {
+        q:'erasable programmable read-only memory" memoria regravavel e apagavel por luz.',
+        options:['MRAL','Flash','Light','EPROM'],
+        answer:3
+    },
+
+    {
+        q:'Um tipo de disco rigido sem partes móveis.',
+        options:['HD','Flash','SSD','Processador'],
+        answer:1
+    },
+
+    {
+        q:'Memórias não voláteis que tem grande capacidade de armazenamento e utilizados para guardar dados permanentemente',
+        options:['Disco','MNV','Memoria de massa','AACD'],
+        answer:2
+    },
+
+    {
+        q:'É um recurso da placa mãe que capacita os periféricos a terem acesso direto à memória RAM, sem sobrecarregarem o processador',
+        options:['DMA','PMRAM','SSD','Cache'],
+        answer:0
+    },
+
+    {
+        q:'É um mecanismo fundamental para permitir à CPU acessar memórias e/ou periféricos externos',
+        options:['HDD','CS','EPI','Cache'],
+        answer:1
+    },
+
+    {
+        q:'Ajuda a transferir endereços de memoria',
+        options:['Memory transfer addres','MDA','Addres Bus','databus'],
+        answer:2
+    },
+
+    {
+        q:'É usado para receber e enviar dados',
+        options:['Get/out data','Info','DNS','databus'],
+        answer:3
+    },
+
+    {
+        q:'Processador que foi lançado em setembro de 2009. Ele possui quatro núcleos, cache L3 de 8MB e frequência de 2,66GHz.',
+        options:['Core i5','AMD ryzen 5','Core i9','Pentium gold'],
+        answer:0
+    },
+
+    {
+        q:'Seu lançamento foi em 2008, possui 4 núcleos, cache L3 e frequência de 2,80GHz',
+        options:['AMD Ryzen 7','Core i7','Core i5','Pentium gold'],
+        answer:1
+    },
+
+    {
+        q:'Nomenclatura que se dá ao processador que possui dois nucleos, é o mais simples atualmente e é recomendado pra rodar jogos bem levinhos e pras tarefas mais básicas',
+        options:['Bi core','Duplo core','Dual core','2Xcore'],
+        answer:2
+    },
+
+    {
+        q:'Nomenclatura que se dá ao processador que possui quatro núcleos e trabalha com todos eles ao mesmo tempo, executando várias funções',
+        options:['Quartcore','4Xcore','quadraCore','Quad core'],
+        answer:3
+    }
+]
